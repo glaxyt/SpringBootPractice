44bac553979296084fb16e842cd2295111612a8e
glaxyt <glaxyt@naver.com>
2024-02-12 01:51:46 +0900

    day4_2

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -6,8 +6,12 @@ import com.example.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
 
 @Slf4j //logging 기능을 위한 어노테이션 추가
 @Controller
@@ -34,4 +38,23 @@ public class ArticleController {
         return "";
     } // new.mustache에서 전송받은 폼 데이터가 post형식으로 전달 되었기에 이를 받아주는 controller 코드 작성.
     // 이를 위해 dto(data transfer object)에 전달된 폼 데이터를 담는다.
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model){
+        log.info("id= " + id);
+        // 1. id를 조회해 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        // 2. 모델에 데이터 등록하기
+        model.addAttribute("article", articleEntity);
+        // 3. 뷰 페이지 반환하기
+        return "articles/show";
+    }
+    @GetMapping("/articles")
+    public String index(Model model){
+        // 1. 모든 데이터 가져오기
+        ArrayList<Article> articleEntityList = articleRepository.findAll();
+        // 2. 모델에 데이터 등록하기
+        model.addAttribute("articleList", articleEntityList);
+        // 3. 뷰 페이지 설정하기
+        return "articles/index";
+    }
 }
--- src/main/java/com/example/firstproject/repository/ArticleRepository.java
+++ src/main/java/com/example/firstproject/repository/ArticleRepository.java
@@ -2,6 +2,10 @@ package com.example.firstproject.repository;
 
 import com.example.firstproject.entity.Article;
 import org.springframework.data.repository.CrudRepository;
-public interface ArticleRepository extends CrudRepository<Article, Long> {
 
+import java.util.ArrayList;
+
+public interface ArticleRepository extends CrudRepository<Article, Long> {
+    @Override
+    ArrayList<Article> findAll(); // Iterable -> ArrayList 수정
 }
