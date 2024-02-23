9a5310eeded3fef7b99402573781e70af18c09d6
glaxyt <glaxyt@naver.com>
2024-02-23 23:59:39 +0900

    dayalpha

--- src/main/java/com/example/firstproject/api/ArticleApiController.java
+++ src/main/java/com/example/firstproject/api/ArticleApiController.java
@@ -53,6 +53,10 @@ public class ArticleApiController {
 
     @PostMapping("/api/transaction-test")
     public ResponseEntity<List<Article>> transactionTest(@RequestBody List<ArticleForm> dtos){
+        List<Article> createdList = articleService.createArticles(dtos);
+        return (createdList != null) ?
+                ResponseEntity.status(HttpStatus.OK).body(createdList) :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 
     }
 }
